80a7e270dd8a68ee7d71c43817ed25efd219c382
ChaseHBehrens <chasehbehrens@gmail.com>
2026-04-16 14:04:36 -0400

    fn for upadating vals are in changingGait state file

--- unitree_guide2/src/FSM/State_ChangingGait.cpp
+++ unitree_guide2/src/FSM/State_ChangingGait.cpp
@@ -1,5 +1,4 @@
-#include <cassert>
-
+#include "common/mathTypes.h"
 
 /**
  * @param initVal - the value at the start of the transition (0 to 1)
@@ -10,11 +9,30 @@
  */
 double updateVal(double initVal, double targVal, int currStep, int totalSteps) {
     assert(initVal >= 0.0 && initVal <= 1.0);
-    assert(currVal >= 0.0 && currVal <= 1.0);
     assert(targVal >= 0.0 && targVal <= 1.0);
     double totalDiff = targVal - initVal;
     double stepSize = totalDiff / totalSteps;
     return initVal + ((currStep + 1) * stepSize);
 }
 
+// I think this would be computationally easier
+double stepSize(int n, double initVal, double targVal) {
+    assert(initVal >= 0.0 && initVal <= 1.0);
+    assert(targVal >= 0.0 && targVal <= 1.0);
+    double totalDiff = targVal - initVal;
+    return totalDiff / n;
+}
 
+double updateVal(double currVal, double stepSize) {
+    return currVal + stepSize;
+}
+
+// here with vecs
+Vec4 stepSizes(int n, Vec4 initVals, Vec4 targVals) {
+    Vec4 totalDiff = targVals - initVals;
+    return totalDiff / n;
+}
+
+Vec4 updateVals(Vec4 currVals, Vec4 stepSize) {
+    return currVals + stepSize;
+}
