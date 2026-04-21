6d2038be67de13867c73f97a87418bb0510d9c83
ChaseHBehrens <chasehbehrens@gmail.com>
2026-04-21 10:20:42 -0400

    working on it

--- unitree_guide2/src/FSM/State_ChangingGait.cpp
+++ unitree_guide2/src/FSM/State_ChangingGait.cpp
@@ -1,38 +1,67 @@
 #include "common/mathTypes.h"
 
-/**
- * @param initVal - the value at the start of the transition (0 to 1)
- * @param targVal - the value at the end to the transition (0 to 1)
- * @param currStep - the current step of the transition
- * @param totalSteps - the number of steps in the transition
- * @return nextVal
- */
-double updateVal(double initVal, double targVal, int currStep, int totalSteps) {
-    assert(initVal >= 0.0 && initVal <= 1.0);
-    assert(targVal >= 0.0 && targVal <= 1.0);
-    double totalDiff = targVal - initVal;
-    double stepSize = totalDiff / totalSteps;
-    return initVal + ((currStep + 1) * stepSize);
+struct Gait {
+    Vec4 bias;
+    double beta;
+    double period;
+};
+
+void transition(int n, Gait initial, Gait target) {
+    Vec4 biasStepSize = (target.bias - initial.bias) / n;
+    double betaStepSize = (target.beta - initial.beta) / n;
+    double periodStepSize = (target.period - initial.period) / n;
 }
 
-// I think this would be computationally easier
-double stepSize(int n, double initVal, double targVal) {
-    assert(initVal >= 0.0 && initVal <= 1.0);
-    assert(targVal >= 0.0 && targVal <= 1.0);
-    double totalDiff = targVal - initVal;
-    return totalDiff / n;
+#include <chrono>
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
+
+class TimedPublisher : public rclcpp::Node
+{
+    public:
+        TimedPublisher() : Node("timed_publisher"), count_(0) {
+            publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+            timer_ = this->create_wall_timer(
+                    500ms,
+                    std::bind(&TimedPublisher::timer_callback, this)
+                    );
+        }
+
+    private:
+        int i = 0;
+        int n = 100; 
+        Vec4 biasStepSize;
+        double betaStepSize;
+        double periodStepSize;
+        Gait currGait;
+
+        void timer_callback() {
+            if (i < n) {
+                currGait.bias += biasStepSize;
+                currGait.beta += betaStepSize;
+                currGait.period += periodStepSize;
+                i++;
+            }
+
+            auto message = std_msgs::msg::String();
+            message.data = "Hello, ROS2! count: " + std::to_string(count_++);
+            RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+            publisher_->publish(message);
+        }
+
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+        size_t count_;
+};
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<TimedPublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
 
-double updateVal(double currVal, double stepSize) {
-    return currVal + stepSize;
-}
 
-// here with vecs
-Vec4 stepSizes(int n, Vec4 initVals, Vec4 targVals) {
-    Vec4 totalDiff = targVals - initVals;
-    return totalDiff / n;
-}
-
-Vec4 updateVals(Vec4 currVals, Vec4 stepSize) {
-    return currVals + stepSize;
-}
