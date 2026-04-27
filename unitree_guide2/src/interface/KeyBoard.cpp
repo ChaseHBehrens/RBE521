427d7f6da1b1dbd56536a35a090dca94882c68ff
kracka731 <kracka@wpi.edu>
2026-04-27 12:34:40 -0400

    walk, amble

--- unitree_guide2/src/interface/KeyBoard.cpp
+++ unitree_guide2/src/interface/KeyBoard.cpp
@@ -80,13 +80,20 @@ UserCommand KeyBoard::checkCmd(){
     case '8':
         return UserCommand::L1_Y;
 #ifdef RUN_ROS
+    // format: beta (duty factor), period, bias (relative phase)
+    case 'b':
+        publishGaitCmd(0.75, 0.8, 0, 0.5, 0.75, 0.25, "Walk");
+        return UserCommand::NONE;
+    case 'm':
+        publishGaitCmd(0.7, 0.7, 0, 0.5, 0.8, 0.3, "Amble");
+        return UserCommand::NONE;
     case 't':
         publishGaitCmd(0.6, 0.6, 0, 0.5, 0.5, 0, "Trot");
         return UserCommand::NONE;
     case 'p': 
         publishGaitCmd(0.6, 0.6, 0, 0.5, 0, 0.5, "Pace");
         return UserCommand::NONE;
-    case 'j':
+    case 'r':
         publishGaitCmd(0.4, 0.7, 0, 0, 0, 0, "Pronk");
         return UserCommand::NONE;
 #endif
